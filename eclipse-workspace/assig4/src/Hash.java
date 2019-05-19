9e96cfb3fe2b8d126c38dfacff0ff90465ea7100
May <blonda9000@gmail.com>
2019-05-19 22:16:00 +0300

    reading files

---
+++ eclipse-workspace/assig4/src/Hash.java
@@ -0,0 +1,17 @@
+public class Hash{
+	int a;
+	int b;
+	
+	public Hash(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int getA(){
+		return a;
+	}
+	
+	public int getB(){
+		return b;
+	}
+}
