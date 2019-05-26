3065bca9f373d6580fa674137bfef65139d9a995
May <blonda9000@gmail.com>
2019-05-26 19:57:20 +0300

    :)

--- eclipse-workspace/assig4/src/Runner.java
+++ eclipse-workspace/assig4/src/Runner.java
@@ -26,20 +26,20 @@ public class Runner {
 		String rejectedPasswordsAmount = bloomFilter.getRejectedPasswordsAmount(System.getProperty("user.dir")+"/requested_passwords.txt");
 
 		//Create the B tree using the t value and the path to the bad_passwords file.
-		//BTree btree = createTree(args[2]);
+		BTree btree = createTree(args[2]);
 
 
 		//Get the DFS representation of the btree
-		//String treeLayout = btree.toString();
+		String treeLayout = btree.toString();
 
 		//Get the time required to perform a search of all requested words when using the B tree and hash table.
-		//String searchTime = getSearchTime(hashTable, btree);
+		String searchTime = getSearchTime(hashTable, btree);
 
 		//Get the DFS representation of the btree, after performing deletions
-		//String treeLayoutAfterDeletions = deleteKeysFromTree(btree);
+		String treeLayoutAfterDeletions = deleteKeysFromTree(btree);
 
 		//Create a file with the program's expected output
-		//createOutputFile(falsePositivesPercent, rejectedPasswordsAmount, treeLayout, searchTime, treeLayoutAfterDeletions, System.getProperty("user.dir")+"/output.txt");
+		createOutputFile(falsePositivesPercent, rejectedPasswordsAmount, treeLayout, searchTime, treeLayoutAfterDeletions, System.getProperty("user.dir")+"/output.txt");
 	    
 	}
 	
@@ -61,27 +61,27 @@ public class Runner {
 
 	// Create the BTree using the t value, and the friends file.
 	// Insert the bad passwords into the tree.
-	/*private static BTree createTree(String tVal) {
+	private static BTree createTree(String tVal) {
 		BTree btree = new BTree(tVal);
 		btree.createFullTree(System.getProperty("user.dir")+"/bad_passwords.txt");
 		return btree;
-	}*/
+	}
 
-	/*private static String getSearchTime(HashTable hashTable, BTree btree) {
+	private static String getSearchTime(HashTable hashTable, BTree btree) {
 		//Get the time required to perform a search of all requested words when using the B tree.
 		String btreeSearchTime = btree.getSearchTime(System.getProperty("user.dir")+"/requested_passwords.txt");
 		//Get the time required to perform a search of all requested words when using the hash table.
 		String hashTableSearchTime = hashTable.getSearchTime(System.getProperty("user.dir")+"/requested_passwords.txt");
 		return btreeSearchTime+"_"+hashTableSearchTime;
-	}*/
+	}
 
-	/*private static String deleteKeysFromTree(BTree btree) {
+	private static String deleteKeysFromTree(BTree btree) {
 		btree.deleteKeysFromTree(System.getProperty("user.dir")+"/delete_keys.txt");
 		return btree.toString();
-	}*/
+	}
 
 	//Creates a file with the program's expected output
-	/*private static void createOutputFile(String falsePositivesPercent, String rejectedPasswordsAmount, String treeLayout, String searchTime, String treeLayoutAfterDeletions, String pathToOutput) {
+	private static void createOutputFile(String falsePositivesPercent, String rejectedPasswordsAmount, String treeLayout, String searchTime, String treeLayoutAfterDeletions, String pathToOutput) {
 		Path path = Paths.get(pathToOutput);
 		StringBuilder sb = new StringBuilder();
 		sb.append(falsePositivesPercent).append(System.lineSeparator()).append(rejectedPasswordsAmount).append(System.lineSeparator()).append(treeLayout).append(System.lineSeparator()).append(searchTime).append(System.lineSeparator()).append(treeLayoutAfterDeletions);
@@ -91,7 +91,5 @@ public class Runner {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}*/
-
-
+	}
 }
